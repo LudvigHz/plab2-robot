6b3eda51beb06653b26330cf0e8cb447e9961dbb
stianbm <stian.malmanger@gmail.com>
2019-10-28 15:37:27 +0100

    Implemented DontCrashBehavior

--- robot/dont_crash_behavior.py
+++ robot/dont_crash_behavior.py
@@ -7,21 +7,33 @@ class DontCrashBehavior(Behavior):
     """Behavior that uses the UltraSonic Sensob to not crash"""
 
     # Config
-    _threshold_distance = 20
+    _threshold_distance = 30
+    _stop_distance = 10
 
     def __init__(self):
         super().__init__()
         self._halt_request = False
         self._active_flag = False
+        self._motor_recommendations = [0.0, 0.0]
 
     def consider_deactivation(self):
         """If no obstacle is detected, then deactivate"""
-        return
+        for sensob in self._sensobs:
+            for value in sensob.get_values():
+                if value < self._threshold_distance:
+                    return
+        self._active_flag = False
 
     def consider_activation(self):
         """If an obstacle is detected, then activate"""
-        return
+        for sensob in self._sensobs:
+            for value in sensob.get_values():
+                if value < self._threshold_distance:
+                    self._active_flag = True
+                    return
 
     def sense_and_act(self):
-        """Calculate weight, generate motor recommendations, don't generate halt requests"""
-        return
+        """Calculate weight, don't generate halt requests"""
+        self._weight = (
+            (sum(self._raw_values) / len(self._raw_values)) - self._stop_distance
+        ) * self._priority

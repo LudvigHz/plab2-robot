2a49e2cadab4c32b43d544877739c9831870f653
Simen Sverdrup-Thygeson <simenthygeson@gmail.com>
2019-10-29 15:14:24 +0100

    Implemented color detection, probably needs tweaking

--- robot/Imager.py
+++ robot/Imager.py
@@ -1,4 +1,8 @@
+'''File containing the Imager class'''
+
 from PIL import Image
+from PIL import ImageFilter
+from PIL import ImageEnhance
 
 
 class Imager():
@@ -23,6 +27,7 @@ class Imager():
             height=100,
             background='black',
             mode='RGB'):
+        '''Initializes the class'''
         self.init_file_info(fname, dir, ext)
         self.image = image  # A PIL image object
         self.xmax = width
@@ -31,16 +36,19 @@ class Imager():
         self.init_image(background=background)
 
     def init_file_info(self, fname=None, dir=None, ext=None):
+        '''Info on picture file'''
         self.dir = dir if dir else self._image_dir_
         self.ext = ext if ext else self._image_ext_
         self.fid = self.gen_fid(fname) if fname else None
 
     def gen_fid(self, fname, dir=None, ext=None):
+        '''Generates file ID'''
         dir = dir if dir else self.dir
         ext = ext if ext else self.ext
         return dir + fname + "." + ext
 
     def init_image(self, background='black'):
+        '''Initializes image'''
         if self.fid:
             self.load_image()
         if self.image:
@@ -58,29 +66,42 @@ class Imager():
         # argument used.
 
     def dump_image(self, fid, type='gif'):
+        '''Save image'''
         fname = fid.split('.')
         type = fname[1] if len(fname) > 1 else type
         self.image.save(fname[0] + '.' + type, format=type)
 
-    def get_image(self): return self.image
+    def get_image(self):
+        '''Returns image'''
+        return self.image
 
-    def set_image(self, im): self.image = im
+    def set_image(self, im):
+        '''Sets image'''
+        self.image = im
 
-    def display(self): self.image.show()
+    def display(self):
+        '''Displays image'''
+        self.image.show()
 
     def get_image_dims(self):
+        '''Get the image dimensions'''
         self.xmax = self.image.size[0]
         self.ymax = self.image.size[1]
 
     def gen_plain_image(self, w, h, color, mode='RGB'):
+        '''Generate a plain image'''
         return Image.new(mode, (w, h), self.get_color_rgb(color))
 
-    def get_color_rgb(self, colorname): return Imager._pixel_colors[colorname]
+    def get_color_rgb(self, colorname):
+        '''Converts color'''
+        return Imager._pixel_colors[colorname]
 
     def resize(self, new_width, new_height):
+        '''Resize image'''
         return Imager(image=self.image.resize((new_width, new_height)))
 
     def scale(self, xfactor, yfactor):
+        '''Scale image'''
         return self.resize(
             round(
                 xfactor *
@@ -88,3 +109,43 @@ class Imager():
             round(
                 yfactor *
                 self.ymax))
+
+    def get_pixel(self, x, y):
+        '''Get pixel'''
+        return self.image.getpixel((x, y))
+
+    def set_pixel(self, x, y, rgb):
+        '''Set pixel'''
+        self.image.putpixel((x, y), rgb)
+
+    def combine_pixels(self, p1, p2, alpha=0.5):
+        '''Combines pixels with factor alpha'''
+        return tuple([round(alpha * p1[i] + (1 - alpha) * p2[i])
+                      for i in range(3)])
+
+    def map_image(self, func):
+        '''Map image'''
+        return Imager(
+            image=Image.eval(
+                self.image,
+                func))  # eval creates a new image.
+
+    def map_image2(self, func):
+        '''Image mapper helper function'''
+        im2 = self.image.copy()
+        for i in range(self.xmax):
+            for j in range(self.ymax):
+                im2.putpixel((i, j), func(im2.getpixel((i, j))))
+        return Imager(image=im2)
+
+    def map_color_wta(self, thresh=0.34):
+        '''"Winner takes all"-map of image'''
+        def wta(p):  # p is an RGB tuple
+            '''Local function'''
+            s = sum(p)
+            w = max(p)
+            if s > 0 and w / s >= thresh:
+                return tuple([(x if x == w else 0) for x in p])
+            else:
+                return (0, 0, 0)
+        return self.map_image2(wta, self.image)
--- robot/detect_color.py
+++ robot/detect_color.py
@@ -2,34 +2,32 @@
 
 from robot.behavior import Behavior
 from robot.sensors.camera import Camera
-# imager2.py
-from PIL import Image
-from PIL import ImageFilter
-from PIL import ImageEnhance
+from robot import Imager
+from .sensors.motors import Motors
 
 
 class DetectColor(Behavior):
     '''Class for detecting color using the Raspicam'''
-    _pixel_colors = {
-        'red': (
-            255, 0, 0), 'green': (
-            0, 255, 0), 'blue': (
-                0, 0, 255), 'white': (
-                    255, 255, 255), 'black': (
-                        0, 0, 0)}
+    _color_detected = None
     _camera = None
     _value = None
     _color = None  # Color to detect
+    _imager = None  # Imager helper-class
+    _timer = None
 
-    def __init__(self, color, bbcon, priority, sensobs):
+    def __init__(self, colorname, bbcon, priority, sensobs):
         '''Color input determines which color the camera should detect: red, green, blue, white or black'''
         super().__init__(bbcon, priority, sensobs)
         self._halt_request = False
         self._active_flag = False
-        self._motor_recommendations = [
-            1.0, 0.0]  # recommended to spin, change?
+        self._motor_recommendations = [0.0, 0.0]  # recommended to stop
+        self._halt_request = False
+        self._color_detected = False
+        self._weight = 0
         self._camera = Camera()
-        self._color = self._pixel_colors.get(color)
+        self._imager = Imager.Imager()
+        self._color = self._imager.get_color_rgb(colorname)
+        self._timer = 0
 
     def reset(self):
         '''Sets the value slot to None'''
@@ -41,10 +39,53 @@ class DetectColor(Behavior):
         '''Set new color to detect'''
         self._color = color
 
-    def take_picture(self):
-        '''Takes a picture and updates _value'''
+    def take_picture(self, threshold=0.3):
+        '''Takes a picture and updates _value, threshold is the percentage of the pixels which should match _color'''
         self._camera.update()
         self._value = self._camera.get_value()  # returns Image-object
-        self.analyze_picture()
+        self._color_detected = self.analyze_picture(threshold)
 
-    def analyze_picture(self):
+    def analyze_picture(self, threshold):
+        '''Returns a boolean, determining if the picture is a match with _color'''
+        mapped_image = self._imager.map_color_wta()
+        # iterate through the entire image matrix and check pixel color against
+        # local _color variable
+        width = self._imager.xmax
+        height = self._imager.ymax
+        counter = 0
+        for pixel in range(height):
+            for pixel2 in range(width):
+                pixel_color = self._imager.get_pixel(pixel, pixel2)
+                if pixel_color == self._color:
+                    counter += 1
+        self._match_degree = counter / (width * height)
+        return self._match_degree > threshold
+
+    def consider_deactivation(self):
+        '''Deactivate if active, take pictures in intervals'''
+        self._active_flag = False
+
+    def consider_activation(self):
+        '''Activate if _timer is high enough (take pictures in intervals of 100*delay)'''
+        if self._timer > 100:
+            self._active_flag = True
+            self._timer = 0
+        else:
+            self._timer += 1
+
+    def _sense_and_act(self):
+        '''Calculate weight'''
+        self.take_picture()
+        self._weight = self._priority * self._match_degree
+
+    def update(self):
+        """The main interface between the bbcon and the behavior:
+        1 - Update the activity status
+        2 - Call sense and act
+        3 - Update the behavior’s weight
+        """
+        if self._active_flag:
+            self.consider_deactivation()
+        else:
+            self.consider_activation()
+        self._sense_and_act()
